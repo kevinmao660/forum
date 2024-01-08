efd88285177da4877c980a40e85b6e2e8a8ac65f
Kevin Mao <kevin.mao@duke.edu>
2024-01-08 15:24:28 -0500

    username

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -30,8 +30,8 @@ const handler = NextAuth({
         if (!userExists) {
           await User.create({
             email: profile.email,
-            username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.picture,
+            username: profile.name.replace(/\s/g, "").toLowerCase(),
+            image: profile.picture
           });
         }
 
