d4e1e83d05c4f8e7fd993c5823e59dbeaeee8c31
Kevin Mao <kevin.mao@duke.edu>
2024-01-08 14:32:15 -0500

    reverted changes

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -69,7 +69,6 @@ const Feed = () => {
 
   return (
     <section className='feed'>
-      <form className='relative w-full flex-center'>
         <input
           type='text'
           placeholder='Search for a tag or a username'
@@ -78,7 +77,6 @@ const Feed = () => {
           required
           className='search_input peer'
         />
-      </form>
 
       {/* All Events */}
       {searchText ? (
